4dd8aaa55b9e8ef18aba8085815b887011bced12
robertomaldonado <rbt.maldonado@gmail.com>
2020-05-05 23:04:12 +0000

    Unit testing for calculator, sum and subtraction

---
+++ test_calculator.py
@@ -0,0 +1,14 @@
+"""
+Unit tests for calculator
+"""
+import calculator
+ 
+class TestCalculator:
+  def test_addition(self):
+    assert 4 == calculator.add(1, 3)
+    assert 15000 == calculator.add(10000, 5000)
+    assert 0 == calculator.add(-10, 10)
+    
+
+  def test_subtraction(self):
+    assert 8 == calculator.subtract(10, 2)
